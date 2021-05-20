82fbc436e35d4d98276b8bca76af493e684cf217
cserajib <cse.rajib@gmail.com>
2021-05-20 20:24:32 +0530

    Lombok spring boot with junit Test

---
+++ msstudent-master/src/main/java/com/crud/msstudent/MsstudentApplication.java
@@ -0,0 +1,19 @@
+package com.crud.msstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsstudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsstudentApplication.class, args);
+	}
+
+}
+
+
+/*
+ * { "firstname":"rajib", "lastname":"lochan1", "email":"howrah.rajib@gmail.com"
+ * }
+ */
